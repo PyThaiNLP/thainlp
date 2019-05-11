70df818d3f5d379bd26eb0074c78fb2265a04ec7
Wannaphong <wannaphong@kkumail.com>
2019-05-11 17:40:40 +0700

    Update README

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 thainlp is [PyThaiNLP](https://github.com/PyThaiNLP/pythainlp).
 
+Google colab :  [https://colab.research.google.com/drive/1BSjpmQaqmU66F7UGCmKgJHX_zqM_Thu-](https://colab.research.google.com/drive/1BSjpmQaqmU66F7UGCmKgJHX_zqM_Thu-)
+
 ## Install
 
 pip install thainlp
--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 ﻿# -*- coding: utf-8 -*-
 
 from setuptools import find_packages, setup
+with open("README.md", "r", encoding="utf-8") as readme_file:
+    readme = readme_file.read()
 
 requirements = ['pythainlp>=2.0.1']
 
@@ -8,11 +10,12 @@ setup(
     name="thainlp",
     version="0.4.2",
     description="Thai NLP library",
-    long_description="Hello",
+    long_description=readme,
     long_description_content_type="text/markdown",
     author="thainlp",
     packages=find_packages(),
     package_data={},
+    url="https://github.com/wannaphongcom/thainlp",
     include_package_data=True,
     install_requires=requirements,
     license="Apache Software License 2.0",
