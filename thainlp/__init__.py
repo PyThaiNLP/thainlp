c5db20114318360db5b27c383b1333e20580a361
Wannaphong <wannaphong@kkumail.com>
2019-05-11 20:15:06 +0700

    Add WordList

--- thainlp/__init__.py
+++ thainlp/__init__.py
@@ -2,23 +2,29 @@
 import pythainlp
 from pythainlp.transliterate import romanize as romanize_pythainlp
 from collections import Counter
+class WordList(list):
+    def __init__(self, collection):
+        super(WordList, self).__init__(collection)
+    def __str__(self):
+        return '|'.join(self)
+
 class nlp(object):
     def __init__(self,text,dictlist=[]):
         self.text=text
         self.dictlist=dictlist
         if self.dictlist==[]:
-            self.words=pythainlp.word_tokenize(self.text)
+            self.words=WordList(pythainlp.word_tokenize(self.text))
         else:
-            self.dcit=pythainlp.tokenize.dict_trie(self.dictlist)
-            self.words=pythainlp.word_tokenize(self.text,self.dcit)
+            self.dict=pythainlp.tokenize.dict_trie(self.dictlist)
+            self.words=WordList(pythainlp.word_tokenize(self.text,self.dict))
         self.tags=pythainlp.pos_tag(self.words)
         self.romanize=[romanize_pythainlp(i) for i in self.words]
         self.word_counts=Counter(self.words)
     def change_word_tokenize(self,name):
         if self.dictlist==[]:
-            self.words=pythainlp.word_tokenize(self.text,engine=name)
+            self.words=WordList(pythainlp.word_tokenize(self.text,engine=name))
         else:
-            self.words=pythainlp.word_tokenize(self.text,self.dcit)
+            self.words=WordList(pythainlp.word_tokenize(self.text,self.dict))
         self.tags=pythainlp.pos_tag(self.words)
     def change_pos_tag(self,name):
         self.tags=pythainlp.pos_tag(self.words,engine=name)
